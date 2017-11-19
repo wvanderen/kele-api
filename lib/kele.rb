668f71c197ee6966bdd4e246d0d41f8ded581675
William Van Deren <wvanderen@gmail.com>
2017-11-19 17:28:24 +0000

    4-Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,7 +6,6 @@ class Kele
     include HTTParty
 
     def initialize (email, password)
-
         response = self.class.post(base_url("sessions"),
         body:  {
             'email': email,
@@ -20,6 +19,12 @@ class Kele
     
     def get_me
         response = self.class.get(base_url("users/me"), headers: { "authorization" => @auth_token })
+        @json = JSON.generate(response)
+        JSON.parse(@json)
+    end
+    
+    def get_mentor_availability(mentor_id)
+        response = self.class.get(base_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
         @json = JSON.generate(response)
         JSON.parse(@json)
     end
